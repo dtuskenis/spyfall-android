2c3e3a849ed9127fd10c648ab07e09e91069cae4
Denis Tuskenis <denistuskenis@gmail.com>
2021-06-19 10:35:38 +0300

    Add CivilRole to proguard since it is used by navigation

--- app/src/main/kotlin/com/denistuskenis/spyfall/ui/destinations/room/CivilRole.kt
+++ app/src/main/kotlin/com/denistuskenis/spyfall/ui/destinations/room/CivilRole.kt
@@ -1,9 +1,11 @@
 package com.denistuskenis.spyfall.ui.destinations.room
 
 import android.os.Parcelable
+import androidx.annotation.Keep
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
+@Keep
 data class CivilRole(
     val role: String,
     val locationName: String,
